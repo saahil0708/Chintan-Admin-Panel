79e8b473c28a27ebda0032548d0e755c359cd73e
saahil0708 <saahil.ind.dgp@gmail.com>
2025-06-19 19:55:50 +0530

    Change

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
-    const backendURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+    const backendURL = "https://chintan-server.vercel.app" || "http://localhost:5000";
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userData, setUserData] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
